879190c59bfe67b4c682c9c44a60bf73e82329c3
flatypus [mac] <yhc3141@gmail.com>
2023-12-08 12:06:50 -0800

    day 5 1

--- 2023/5/1.py
+++ 2023/5/1.py
@@ -5,7 +5,7 @@ file = read("../..").split('\n\n')
 seeds, instructions = file[0], file[1:]
 
 seeds = intify(re.findall(r'\d+', seeds))
-seeds = [(seeds[i], seeds[i+1]) for i in range(0, len(seeds), 2)]
+seeds = [(seeds[i], seeds[i] + seeds[i+1]) for i in range(0, len(seeds), 2)]
 
 maps = []
 
@@ -16,14 +16,29 @@ for i in instructions:
 
     m = {}
     for new, start, length in i:
-        m[start] = (new, length)
+        m[start] = (new, start + length)
 
     maps.append(m)
-
-print(maps)
 
 
 n = float("inf")
 
+for seed_start, seed_end in seeds:
+    print("Seed start:", seed_start, "Seed end:", seed_end)
+    ranges = [(seed_start, seed_end)]
+    for instruction_set in maps:
+        print(ranges)
+        new_ranges = []
+        instructions = list(instruction_set.items())
+        found = False
+        for seed_start, seed_end in ranges:
+            item = 0
+            print("Seed start:", seed_start, "Seed end:", seed_end)
+            while item < len(instructions):
+                start, (new, end) = instructions[item]
 
-print(seeds)
+        ranges = new_ranges.copy()
+    if len(ranges) > 0 and min(ranges)[0] < n:
+        n = min(ranges)[0]
+
+print(n)
